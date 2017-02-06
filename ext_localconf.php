76120bb612903b23d745b051f7296e81b924da0c
Andreas Kiessling <kiessling@pluspol-interactive.de>
2017-02-06 14:35:54 +0100

    Use ::class constant and only set variable in backend context

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,4 +1,6 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderTopToolbar']['ppi_templavoila_notes'] = 'Ppi\PpiTemplavoilaNotes\Hooks\TopToolbar->render';
+if (TYPO3_MODE === 'BE') {
+    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderTopToolbar']['ppi_templavoila_notes'] = \Ppi\PpiTemplavoilaNotes\Hooks\TopToolbar::class . '->render';
+}
