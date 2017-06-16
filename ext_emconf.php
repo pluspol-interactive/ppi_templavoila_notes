1ae9a9a665370eb4caafda463a852eb7a3f14944
Alexander Opitz <opitz@pluspol.info>
2017-06-16 17:30:25 +0200

    [FEATURE] Mark as TYPO3 v8 LTS compatible

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '0.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0-7.6.99',
+            'typo3' => '7.6.0-8.7.99',
             'sys_note' => '7.6.0-7.6.99',
             'templavoilaplus' => '7.1.2-'
         ],
