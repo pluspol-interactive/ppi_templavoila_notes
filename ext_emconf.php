8c5704a851b58dec5d96cc6a5042a5e5e7bd8a49
Alexander Opitz <opitz@pluspol.info>
2019-04-11 14:32:10 +0200

    [RELEASE] Release 0.4.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,20 +1,20 @@
 <?php
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'TemplaVoila! Plus - Notes',
+    'title' => 'TemplaVoilà! Plus - Notes',
     'description' => 'Show sys_note records in TemplaVoilà! Plus page module',
     'category' => 'plugin',
-    'version' => '0.3.0',
+    'version' => '0.4.0',
     'state' => 'stable',
     'uploadfolder' => '0',
     'clearCacheOnLoad' => 0,
     'author' => 'Alexander Opitz',
-    'author_email' => 'alexander.opitz@pluspol-interactive.de',
+    'author_email' => 'opitz@pluspol-interactive.de',
     'author_company' => 'PLUSPOL interactive',
     'constraints' => [
         'depends' => [
             'php' => '5.5.0-7.2.99',
-            'typo3' => '7.6.0-8.7.99',
-            'sys_note' => '7.6.0-8.7.99',
+            'typo3' => '7.6.0-9.5.99',
+            'sys_note' => '7.6.0-9.5.99',
             'templavoilaplus' => '7.1.2-7.99.99'
         ],
     ],
